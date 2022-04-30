153a7fe0f07c9274d9bde40463a1b0232aec5220
tetuaoro <tetuaoropro@gmail.com>
2022-04-29 15:51:55 -1000

    fix: export
    * `next export` does not support the use of next/image without Vercel
    * use <img /> tag instead

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextComponentType, NextPage } from "next"
 import Head from "next/head"
 import Link from "next/link"
-import Image from "next/image"
 import { Fragment } from "react"
 
 export const Navigation: NextComponentType = () => {
@@ -37,7 +36,7 @@ export const Footer: NextComponentType = () => {
       <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
         Powered by{" "}
         <span>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
       </a>
     </footer>
